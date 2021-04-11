ce14042e87d68f96aaecf365f2f612c077e24736
Michael <Michael@believeinrealtiy.com>
2021-04-11 13:03:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 import os
 import csv
 import math
-import subprocess
 import pathlib
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -39,7 +38,7 @@ for i in filenames:
                                                                "--unit-height m --unit-rotation deg/s " \
                                                                "--unit-acceleration g --unit-gps-speed kph " \
                                                                "--merge-gps --declination-dec 7.03 " \
-                                                               "--unit-vbat V --debug " + filenames[b]  # Start Blackbox decode
+                                                               "--unit-vbat V --simulate-imu --debug " + filenames[b]  # Start Blackbox decode
                                         # with parameters
             os.system(bbdecode)             # Call blackbox_decode
             print("Blackbox converted successful to csv ")
@@ -61,7 +60,7 @@ for i in filenames:
             numcolumn = len(csv_reader[0]) # determine columns in csv file
             numrow = len(csv_reader)       # determine rows in csv file
             line_count = 0
-            for row in csv_reader:                  # Split up GPS coordinates for each
+            for row in csv_reader:
                 if (line_count % 10000 ) == 0:      # Simple progress indication
                     print('\r' "Calculation progress: " + str(round((float(line_count/numrow*100)), 2)) + " %" ,end="")
                 if line_count == 0:                 # Prepare header row
@@ -91,8 +90,8 @@ for i in filenames:
                                                                        float(csv_reader[line_count][22]), 2)) # Calculate Motor power
                     csv_reader[line_count].insert(numcolumn + 5, "0.0")  # Initial Home distance set to 0
                     csv_reader[line_count].insert(numcolumn + 6, "0.0")  # Initial travel distance set to 0
-                    csv_reader[line_count][49] = csv_reader[25][49]   # Copy of a later GPS position to use at home.
-                    csv_reader[line_count][50] = csv_reader[25][50]  # Copy of a later GPS position to use at home.
+                    csv_reader[line_count][52] = csv_reader[25][52]   # Copy of a later GPS position to use at home.
+                    csv_reader[line_count][53] = csv_reader[25][53]  # Copy of a later GPS position to use at home.
                     csv_reader[line_count].insert(numcolumn + 7, "0.0")  # Initial GPS start speed set to 0
                     csv_reader[line_count].insert(numcolumn + 8, "0.0")  # Initial Gforece max set to 0
                     csv_reader[line_count].insert(numcolumn + 9, "0.0")  # Initial Motor Power max set to 0
@@ -115,37 +114,37 @@ for i in filenames:
                     csv_reader[line_count].insert(numcolumn + 4, round(float(csv_reader[line_count][numcolumn + 1]) *
                                                                       float(csv_reader[line_count][22]), 2)) # Calculate Motor power
 
-                    csv_reader[line_count].insert(numcolumn + 5, round((haversine(float(csv_reader[20][49]),
-                                                                float(csv_reader[20][50]),
-                                                                float(csv_reader[line_count - 1][49]),
-                                                                float(csv_reader[line_count - 1][50]))), 2)
+                    csv_reader[line_count].insert(numcolumn + 5, round((haversine(float(csv_reader[20][52]),
+                                                                float(csv_reader[20][53]),
+                                                                float(csv_reader[line_count - 1][52]),
+                                                                float(csv_reader[line_count - 1][53]))), 2)
                                                   )  # Calculating distance to home using the GPS coordinate form line 20 as the home point
-                    csv_reader[line_count].insert(numcolumn + 6, round((float(haversine(float(csv_reader[line_count][49]),
-                                                                      float(csv_reader[line_count][50]),
-                                                                      float(csv_reader[line_count - 1][49]),
-                                                                      float(csv_reader[line_count - 1][50]))) +
+                    csv_reader[line_count].insert(numcolumn + 6, round((float(haversine(float(csv_reader[line_count][52]),
+                                                                      float(csv_reader[line_count][53]),
+                                                                      float(csv_reader[line_count - 1][52]),
+                                                                      float(csv_reader[line_count - 1][53]))) +
                                                   float(csv_reader[line_count - 1][numcolumn + 5])), 2)
                                                   )  # Using the GPS coordinate to caclculate distance traveled
-                    if float(csv_reader[line_count][52]) > float(csv_reader[line_count - 1][60]):  # Compare max Gps Speed
-                        csv_reader[line_count].insert(numcolumn + 7, csv_reader[line_count][52])
+                    if float(csv_reader[line_count][55]) > float(csv_reader[line_count - 1][numcolumn + 6]):  # Compare max Gps Speed
+                        csv_reader[line_count].insert(numcolumn + 6, csv_reader[line_count][55])
                     else:
-                        csv_reader[line_count].insert(numcolumn + 7, csv_reader[line_count - 1][60])
-                    if float(csv_reader[line_count][56]) > float(csv_reader[line_count - 1][61]):  # Compare max Geforces
-                        csv_reader[line_count].insert(numcolumn + 8, csv_reader[line_count][56])
+                        csv_reader[line_count].insert(numcolumn + 6, csv_reader[line_count - 1][numcolumn + 6])
+                    if float(csv_reader[line_count][numcolumn + 2]) > float(csv_reader[line_count - 1][numcolumn + 7]):  # Compare max Geforces
+                        csv_reader[line_count].insert(numcolumn + 7, csv_reader[line_count][numcolumn + 2])
                     else:
-                        csv_reader[line_count].insert(numcolumn + 8, csv_reader[line_count - 1][61])
-                    if float(csv_reader[line_count][57]) > float(csv_reader[line_count - 1][62]):  # Motor Power max
-                        csv_reader[line_count].insert(numcolumn + 9, csv_reader[line_count][57])
+                        csv_reader[line_count].insert(numcolumn + 7, csv_reader[line_count - 1][numcolumn + 7])
+                    if float(csv_reader[line_count][numcolumn + 3]) > float(csv_reader[line_count - 1][numcolumn + 8]):  # Motor Power max
+                        csv_reader[line_count].insert(numcolumn + 8, csv_reader[line_count][numcolumn + 3])
                     else:
-                        csv_reader[line_count].insert(numcolumn + 9, csv_reader[line_count - 1][62])
-                    if float(csv_reader[line_count][22]) > float(csv_reader[line_count - 1][63]):  # Current draw max
+                        csv_reader[line_count].insert(numcolumn + 8, csv_reader[line_count - 1][numcolumn + 8])
+                    if float(csv_reader[line_count][22]) > float(csv_reader[line_count - 1][numcolumn + 9]):  # Current draw max
                         csv_reader[line_count].insert(numcolumn + 10, csv_reader[line_count][22])
                     else:
-                        csv_reader[line_count].insert(numcolumn + 10, csv_reader[line_count - 1][63])
-                    if float(csv_reader[line_count][numcolumn + 1]) < float(csv_reader[line_count - 1][64]):  # Battery Volatge min
+                        csv_reader[line_count].insert(numcolumn + 10, csv_reader[line_count - 1][numcolumn + 9])
+                    if float(csv_reader[line_count][numcolumn + 1]) < float(csv_reader[line_count - 1][numcolumn + 10]):  # Battery Volatge min
                         csv_reader[line_count].insert(numcolumn + 11, csv_reader[line_count][numcolumn + 1])
                     else:
-                        csv_reader[line_count].insert(numcolumn + 11, csv_reader[line_count - 1][64])
+                        csv_reader[line_count].insert(numcolumn + 11, csv_reader[line_count - 1][numcolumn + 10])
 
                     """csv_reader[line_count][32] = round(float(
                         float(csv_reader[line_count][32]) + 1024.0) / 20.48,2)  # convert Throttle to %"""
